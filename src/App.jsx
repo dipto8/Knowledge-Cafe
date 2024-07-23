ae5dfc2bd3b1ffd6798187ea53d544d4ad306368
FAHAD KABIR <hmdipto7@gmail.com>
2024-07-23 14:29:12 -0230

    layout design-nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
 
-      <h1 className='text-6xl'>Knowledge Cafe</h1>
+      <Header></Header>
+      <div className=" md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
 
   
     </>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import profile from '../../assets/image/profile.png'
+
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center p-4 m-4 border-2'>
+            <h1 className='text-6xl font-bold'>Knowledge Cafe</h1>
+            <img src= {profile} alt="" />
+        </header>
+    );
+};
+
+export default Header;
