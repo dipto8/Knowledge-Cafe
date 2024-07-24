3e7a7b41a657f9589999f0342fef9f7ea851c2ef
FAHAD KABIR <hmdipto7@gmail.com>
2024-07-23 22:06:22 -0230

    UI update Data fetch

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,44 @@
-import PropTypes from 'prop-types'; 
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    console.log(blog)
-    return (
-        <div>
-            
+const Blog = ({ blog }) => {
+  const {
+    Title,
+    Cover,
+    Author,
+    Author_img,
+    Posted_date,
+    Reading_time,
+    Hashtags,
+    Id,
+  } = blog;
+  return (
+    <div>
+      <img src={Cover} alt="" />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-14" src={Author_img} alt="" />
+          <div className="ml-2">
+            <h2 className="text-2xl">{Author}</h2>
+            <p>{Posted_date}</p>
+          </div>
         </div>
-    );
+        <div className="">
+          <span>{Reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-2xl"> Title:{Title}</h2>
+      <p>
+        {Hashtags.map((Hashtag,Idx) => (
+          <span key={Idx}>
+            <a href="">{Hashtag}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 export default Blog;
